3f19dd685aad0bf441b36c6d937b221527f64efd
Steven Young <stevenyoung131@gmail.com>
2017-10-04 22:01:10 -0400

    Add comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Styles variable that holds style data for use in initializing google map
 var styles = [{
     featureType: 'water',
     stylers: [{
@@ -68,6 +69,8 @@ var styles = [{
     }]
 }]
 
+// Model holding location data for use in ViewModel
+// venueId aquired from and used with foursquare API
 var locationsModel = [{
     name: "Food City",
     location: {
@@ -112,20 +115,13 @@ var locationsModel = [{
     venueId: "4e13c4fa6284431b5351b121"
 }]
 
-// var Location = function(data) {
-//     this.name = ko.observable(data.name);
-//     this.lat = ko.observable(data.latitude);
-//     this.lng = ko.observable(data.longitude);
-//     this.id = ko.observable(data.venueId);
-// }
-
 var ViewModel = function() {
     var self = this;
 
+    // Create an empty array for holding marker meta-data
     this.markers = ko.observableArray([]);
 
-    var foursquareData;
-
+    // Initialize google map Object
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
         center: new google.maps.LatLng(35.22, -85.15),
@@ -133,6 +129,8 @@ var ViewModel = function() {
         styles: styles
     });
 
+    // Function bound to reset filter button
+    // Sets all markers as visible and alerts ko.computed filter funciton of the change
     self.reset = function() {
         for (var i = 0; i < markers().length; i++) {
             markers()[i].setVisible(true);
@@ -140,6 +138,8 @@ var ViewModel = function() {
         }
     }
 
+    // Function that causes a marker to bounce and info-window to open
+    // when a location is clicked from the list outside of the map
     self.listClick = function(marker) {
         populateInfoWindow(marker, largeInfoWindow);
         if (bouncingMarker)
@@ -151,22 +151,20 @@ var ViewModel = function() {
             bouncingMarker = null;
     };
 
-    // this.locationList = ko.observableArray([]);
-
-    // locationsModel.forEach(function(locationItem) {
-    //     self.locationList.push(new Location(locationItem));
-    // });
-
+    // Create empty observable that will be filled by a data-binding in the search box in the DOM
     this.filter = ko.observable();
 
+    // Initialize instance of info-window
     var largeInfoWindow = new google.maps.InfoWindow();
 
+    // Set colors for default and highlighted icons
     var defaultIcon = makeMarkerIcon('0091ff');
-
     var highlightedIcon = makeMarkerIcon('FFFF24');
 
+    // Initialize global variable for holding current bouncing marker
     var bouncingMarker = null;
 
+    // Create Marker object for each location in Model
     for (var i = 0; i < locationsModel.length; i++) {
         var position = locationsModel[i].location;
         var title = locationsModel[i].name;
@@ -180,8 +178,10 @@ var ViewModel = function() {
             id: i
         });
 
+        // Push created marker Object to markers array
         markers.push(marker);
 
+        // Add listeners to markers that cause the marker to bounce and open info window when clicked
         marker.addListener('click', function() {
             populateInfoWindow(this, largeInfoWindow);
             if (bouncingMarker)
@@ -193,6 +193,7 @@ var ViewModel = function() {
                 bouncingMarker = null;
         });
 
+        // Listeners that change marker color on hover
         marker.addListener('mouseover', function() {
             this.setIcon(highlightedIcon);
         });
@@ -202,6 +203,8 @@ var ViewModel = function() {
         marker.setMap(map);
     }
 
+    // KO computed function that filters list of locations when filter search box is used
+    // Sets markers for locations removed from the list as setVisible(false)
     this.visiblePlaces = ko.computed(function() {
         return this.markers().filter(function(marker) {
             if (!self.filter() || marker.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1) {
@@ -212,20 +215,27 @@ var ViewModel = function() {
         });
     }, this);
 
+    // Function to populate the info window with information from marker object
+    // as well as populate DOM elements with foursquare location information
     function populateInfoWindow(marker, infowindow) {
+        // Checks to make sure that the clicked marker is a new marker and clears content if not
         if (infowindow.marker != marker) {
             infowindow.setContent('');
             infowindow.marker = marker;
+            // When info-window is closed, also stops marker bounce animation
             infowindow.addListener('closeclick', function() {
                 infowindow.marker = null;
                 marker.setAnimation(null);
             });
             var innerHTML = '<div>';
+            // Sets the location's title in the info window if it exists
             if (marker.title) {
                 innerHTML += '<strong id="info-box">' + marker.title + '</strong>';
             };
+            // Foursquare API authorization information
             var clientId = "2NQRW41L5Q4VX3JUUQXUVBGKOSAKHKWVABQVQ3F1Q4YS0GOB";
             var clientSecret = "U4E21UUZFYH3JU1O1XB02IDWK12YX21HZMCPAFC0L3WLPYBL";
+            // AJAX call to foursquare venue's API
             $.ajax({
                 url: "https://api.foursquare.com/v2/venues/" + marker.venueId,
                 dataType: "json",
@@ -236,6 +246,7 @@ var ViewModel = function() {
                     v: "20180801",
                 },
                 success: function( response ){
+                    // On success, populates DOM with response data
                     var data = response.response.venue;
                     console.log(data);
                     $("#name").text('Name: ' + data.name);
@@ -253,6 +264,7 @@ var ViewModel = function() {
                     }
                 },
                 fail: function(e) {
+                    // Throws an alert if foursquare API call fails
                     alert("There was a problem reaching the foursquare API.  Please try your request again.")
                 }
             });
@@ -262,6 +274,8 @@ var ViewModel = function() {
         }
     }
 
+    // Uses google maps markerImage to make markers based on color passed into function
+    // Used to create two different markers for hovered markers
     function makeMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
             'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
@@ -272,26 +286,9 @@ var ViewModel = function() {
             new google.maps.Size(21, 34));
         return markerImage;
     }
-
-    // function getFoursquareInfo(markerId) {
-    //     var clientId = "2NQRW41L5Q4VX3JUUQXUVBGKOSAKHKWVABQVQ3F1Q4YS0GOB";
-    //     var clientSecret = "U4E21UUZFYH3JU1O1XB02IDWK12YX21HZMCPAFC0L3WLPYBL";
-    //     $.ajax({
-    //         url: "https://api.foursquare.com/v2/venues/" + markerId,
-    //         dataType: "json",
-    //         method: "GET",
-    //         data: {
-    //             client_id: clientId,
-    //             client_secret: clientSecret,
-    //             v: 20180801,
-    //         },
-    //         success: function(data){
-    //             foursqareData = data;
-    //         }
-    //     });
-    // }
 }
 
+// Adding the .on('load') functionality allows the async map call to complete before applying bindings
 $(window).on('load', function() {
     ko.applyBindings(ViewModel);
 });
